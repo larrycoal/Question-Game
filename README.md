083392a4601ca26a12f7b1ed48ddfad3d6329c6a
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-07-16 12:26:04 +0100

    wrote some test

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1 Built using React,Redux,React-Router and Scss
 
 2 Hard coded credentials. Username: gamer and Password: password
+
+3 Live link to app: https://festive-panini-dc3740.netlify.app/
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import ReactDom from 'react-dom'
+import { BrowserRouter } from "react-router-dom"
+import store from './store';
+import {Provider} from 'react-redux'
 import App from './App';
+import playerReducer from './store/Reducer/playerReducer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const AppTest = (
+  <Provider store={store}>
+      <BrowserRouter>
+          <App/>
+      </BrowserRouter>
+  </Provider>
+)
+describe("App Component", () => {
+  let container
+
+  beforeEach(() => {
+      container = document.createElement('div')
+      document.body.appendChild(container)
+      ReactDom.render(AppTest, container)
+  })
+  afterEach(() => {
+      document.body.removeChild(container);
+      container.remove()
+  })
+
+  it("Login renders correctly", () => {
+      const input = document.querySelectorAll('input')
+      const button = document.querySelectorAll('button')
+      expect(input).toHaveLength(2)
+      expect(input[0].type).toBe("text")
+      expect(input[1].type).toBe("password")
+      expect(button).toHaveLength(1)
+     
+  })
+  it("renders Home correctly",()=>{
+    const input = document.querySelectorAll('input')
+    const button = document.querySelectorAll('button')
+    expect(input).toHaveLength(2)
+    expect(button).toHaveLength(1)
+  })
+})
+describe("reducer",()=>{
+  it("login reducer",()=>{
+  let player = playerReducer({isAuthenticated:false},{type:"login"})
+  expect(player).toEqual({isAuthenticated:true})
+  })
+  it("logout reducer",()=>{
+    let player = playerReducer({isAuthenticated:true},{type:"logout"})
+    expect(player).toEqual({isAuthenticated:false})
+    })
+ 
+})
