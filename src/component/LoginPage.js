736c663709cf4f90d84c9af6023ae53c5cda8fec
Olayiwola Olanrewaju <larry_coal@outlook.com>
2021-07-16 13:31:52 +0100

    fixed last guess

--- src/component/LoginPage.js
+++ src/component/LoginPage.js
@@ -3,12 +3,15 @@ import { useDispatch } from 'react-redux';
 import {Login} from '../store/Action'
 const LoginPage = (props) => {
     const[state, setState]=useState({})
+    const [error,setError] =useState()
     const dispatch = useDispatch()
     const handleLogin=(e)=>{
         e.preventDefault()
         if(state.userName === "gamer" && state.password === "password"){
             dispatch(Login())
            props.history.push("/home")
+        }else{
+            setError(true)
         }
     }
     return (
@@ -27,6 +30,9 @@ const LoginPage = (props) => {
                 onChange={(e)=>setState({...state,password:e.target.value})}
                 />
                 <button onClick={handleLogin}>Login</button>
+                {
+                    error?<div className="error">username or Password Incorrect</div>:null
+                }
             </form>
         </div>
     );
